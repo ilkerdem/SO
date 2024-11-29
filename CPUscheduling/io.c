5b1ce1cd2dc0964bd546f4f579edf7974d61a9a0
ilkerdem <ilker.demirkol@upc.edu>
2024-11-29 16:33:46 +0100

    Update io.c

--- CPUscheduling/io.c
+++ CPUscheduling/io.c
@@ -1,86 +1,42 @@
-include <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <unistd.h>
 
-
-
 #define CHUNK_SIZE (10 * 1024 * 1024) // 10 MB
-
 #define FILE_NAME "large_io_test.txt"
 
-
-
 void main() {
-
   unsigned long long i = 0;
-
   FILE *file;
-
   char *data;
 
-
-
   // Allocate a large chunk of memory
-
   data = (char *)malloc(CHUNK_SIZE);
 
-
-
   // Fill the buffer with dummy data
-
   memset(data, 'A', CHUNK_SIZE);
 
-
-
   while (1) {
-
     // Write to a file
-
     /*        file = fopen("simple_io_test.txt", "w");
-
             if (file == NULL) {
-
                 perror("Error opening file");
-
                 return;
-
             }
-
       */
-
     file = fopen(FILE_NAME, "w");
-
     // Write the data to the file
-
     if (fwrite(data, 1, CHUNK_SIZE, file) != CHUNK_SIZE) {
-
       perror("Error writing to file");
-
       fclose(file);
-
       free(data);
-
       return;
-
     }
-
     fclose(file);
 
-
-
     printf("File write # %llu\n", i++);
-
     sleep(0.1);
-
   }
-
   free(data);
-
 }
-
-
-
